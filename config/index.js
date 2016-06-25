5363fa5503cf763ac2876944b6bde0bf5a65e374
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-06-25 22:01:10 +0200

    Initial commit

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
 module.exports = {
-  SITEMAP_URL: process.env.SITEMAP_URL || 'http://www.yoursite.com/sitemap.xml'
+  REPORT_FILE_PATH: process.env.REPORT_FILE_PATH || 'lix-report.xlsx',
+  SITEMAP_URL: process.env.SITEMAP_URL || 'http://www.telemark.no/sitemap.xml'
 }
--- index.js
+++ index.js
@@ -1,39 +1,50 @@
 'use strict'
 
 const http = require('http')
+const lix = require('lix')
+const xlsx = require('xlsx-writestream')
 const smtaStream = require('sitemap-to-array').stream
 const config = require('./config')
 const xrayPage = require('./lib/xray-page')
 const repackContent = require('./lib/repack-content')
-var addIndex = require('./lib/add-index')
-var deleteIndex = require('./lib/delete-index')
 var pages = []
+var measures = []
 
-function indexPages (pages) {
+function measurePages (pages) {
   var list = JSON.parse(JSON.stringify(pages))
 
   function next () {
     if (list.length > 0) {
-      var page = list.pop()
-      xrayPage(page, function (error, data) {
+      const page = list.pop()
+      if (page) {
+        console.log(page)
+        xrayPage(page, function (error, data) {
+          if (error) {
+            console.error(error)
+          } else {
+            var pageData = repackContent(data)
+            pageData.url = page
+            pageData.lix = lix(pageData.content)
+            measures.push({
+              url: pageData.url,
+              title: pageData.title,
+              lix: pageData.lix
+            })
+            next()
+          }
+        })
+      } else {
+        next()
+      }
+    } else {
+      console.log('Finished measuring')
+      xlsx.write(config.REPORT_FILE_PATH, measures, function (error) {
         if (error) {
           console.error(error)
         } else {
-          var content = repackContent(data)
-          content.url = page
-          addIndex(content, function (err, payload) {
-            console.log(page)
-            if (err) {
-              console.error(err)
-            } else {
-              console.log(payload)
-              next()
-            }
-          })
+          console.log('Finished writing file')
         }
       })
-    } else {
-      console.log('Finished indexing')
     }
   }
 
@@ -47,17 +58,10 @@ smtaStream.on('data', function (data) {
 
 smtaStream.on('end', function () {
   console.log('Finished collecting pages')
-  indexPages(pages)
+  measurePages(pages)
 })
 
-deleteIndex(function (error, payload) {
-  if (error) {
-    console.error(error)
-  } else {
-    console.log(payload)
-    http.get(config.SITEMAP_URL, function (response) {
-      response
-        .pipe(smtaStream)
-    })
-  }
+http.get(config.SITEMAP_URL, function (response) {
+  response
+    .pipe(smtaStream)
 })
